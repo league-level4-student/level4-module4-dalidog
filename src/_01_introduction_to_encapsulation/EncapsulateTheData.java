4cca2fc0d1b060abb350a5c03dde227818e80d03
Your Name <you@example.com>
2019-04-28 11:31:53 -0700

    start

--- src/_01_introduction_to_encapsulation/EncapsulateTheData.java
+++ src/_01_introduction_to_encapsulation/EncapsulateTheData.java
@@ -23,10 +23,56 @@ public class EncapsulateTheData {
 	//2. Create a new JUnit Test case and write tests to verify that 
 	//   the member variables' getters and setters are working
 	
-	int itemsReceived; //must not be negative. All negative arguments get set to 0.
-	float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
-	String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
-	Object memberObj; //can be any object type except String. Strings get turned into objects.
+	private int itemsReceived; //must not be negative. All negative arguments get set to 0.
+	private float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
+	private String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
+	private Object memberObj; //can be any object type except String. Strings get turned into objects.
+	public void setitemsReceived(int itemsReceived) {
+		this.itemsReceived=itemsReceived;
+		if(itemsReceived<0) {
+			this.itemsReceived=0;
+		}
+	}
+	public int getitemsReceived(){
+		return this.itemsReceived;
+		
+	}
+	public void setdegreesTurned(float degreesTurned) {
+		this.degreesTurned=degreesTurned;
+		if(degreesTurned<0.0) {
+			this.degreesTurned=(float) 0.0;
+		}
+		if(degreesTurned>360.0) {
+			this.degreesTurned=(float) 360.0;
+		}
+	}
+	public float getdegreesTurned(){
+		return this.degreesTurned;
+		
+	}
+	public void setnomenclature(String nomenclature) {
+		this.nomenclature=nomenclature;
+		if(nomenclature=="") {
+			this.nomenclature=" ";
+		}
+	}
+	public String getnomenclature(){
+		return this.nomenclature;
+		
+	}
+	public void setmemberObj(Object memberObj) {
+		this.memberObj=memberObj;
+		if(memberObj instanceof String) {
+			this.memberObj=(Object) memberObj;
+		}
+	}
+	public Object getmemberObj(){
+		return this.memberObj;
+		
+	}
+	public void testitemsReceived(int itemsReceived) {
+		
+	}
 	
 	public static void main(String[] args) {
 		
